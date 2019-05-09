2c3816b2cbce7c5e21ea769b640644240f5f5bb1
ltx <545367935@qq.com>
2019-05-09 16:02:18 +0800

    第一次添加

---
+++ HelloGit/src/com/itcast/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.itcast.demo;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("a");
+    }
+}
